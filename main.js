c2b8e17bf61d06c680bd5b53f4b7733700680ae1
Ezra Brooks <ezra.m.brooks@gmail.com>
2013-07-19 11:08:17 -0400

    switched from XHR to JSONP

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-function importAPI(){
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://www.govtrack.us/api/v2/role?current=true", false);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      var resp = JSON.parse(xhr.responseText);
-      var respobj = eval(resp)
-      document.write(respobj.objects[1].person.firstname);
-    }
-  }
-  xhr.send();
-  return createdDate;
+function getApi(){
+  var api = document.createElement('script');
+  api.src = 'http://www.govtrack.us/api/v2/role?current=true&format=jsonp&callback=process'
+  document.head.appendChild(api)
 }
-importAPI();
+function process(){
+  document.write(api.objects)
+}
+getApi();
