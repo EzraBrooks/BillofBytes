575817ce7d159a1e5a6a3c5d3503f5c95d43b26f
Ezra Brooks <ezra.m.brooks@gmail.com>
2013-07-19 14:29:55 -0400

    Theming and comments

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 var senatetext = "";
 var housetext = "";
 var billactions = "";
-function getApi(){
+function getApi(){ //Retrieve JSONP API file
   var api = document.createElement('script');
   api.src = 'http://www.govtrack.us/api/v2/bill?congress=112&order_by=-current_status_date&format=jsonp&q=internet'
   document.head.appendChild(api)
 }
-function callback(contents){
-  //contents = eval(contents)
+function callback(contents){ //API callback
   for(var i = 0; i < contents.objects.length; i++){
     for(var j = 0; j < contents.objects[i].major_actions.length; j++){
       billactions = billactions + "<li>" + contents.objects[i].major_actions[j][2] + "</li>"
     }
     if(contents.objects[i].display_number.substr(0,1) == "S"){
-      senatetext = senatetext + "<h3>" + contents.objects[i].display_number + ": " + contents.objects[i].current_status_label + " as of "+ contents.objects[i].current_status_date + ", introduced " + contents.objects[i].introduced_date + ".</h3><h4>" + contents.objects[i].titles[0][2] + "</h4><ul>" + billactions + "</ul>";
+      senatetext = senatetext + "<h3>" + contents.objects[i].display_number + ": " + contents.objects[i].current_status_label + " as of "+ contents.objects[i].current_status_date + ", introduced " + contents.objects[i].introduced_date + ".</h3><h4>" + contents.objects[i].title_without_number + "</h4><ul>" + billactions + "</ul>";
     }
     else if(contents.objects[i].display_number.substr(0,1) == "H"){
-      housetext = housetext + "<h3>" + contents.objects[i].display_number + ": " + contents.objects[i].current_status_label + " as of "+ contents.objects[i].current_status_date + ", introduced " + contents.objects[i].introduced_date + ".</h3><h4>" + contents.objects[i].titles[0][2] + "</h4><ul>" + billactions + "</ul>";
+      housetext = housetext + "<h3>" + contents.objects[i].display_number + ": " + contents.objects[i].current_status_label + " as of "+ contents.objects[i].current_status_date + ", introduced " + contents.objects[i].introduced_date + ".</h3><h4>" + contents.objects[i].title_without_number + "</h4><ul>" + billactions + "</ul>";
     }
     billactions = "";
   }
 }
-function showHouse(){
+function showHouse(){ //Switch page text to House bills
   document.getElementById("data").innerHTML = housetext;
 };
-function showSenate(){
+function showSenate(){ //Switch page text to Senate bills
   document.getElementById("data").innerHTML = senatetext;
 }
-getApi();
+getApi(); //begin script
