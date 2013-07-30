c7adcc36cec0b0e0aac19b14894fef97a2d41f37
Ezra Brooks <ezra.m.brooks@gmail.com>
2013-07-30 16:35:38 -0400

    Consolidation, Theming, Copyright

--- main.js
+++ main.js
@@ -13,19 +13,16 @@ function callback(contents){ //API callback
     for(var j = 0; j < contents.objects[i].major_actions.length; j++){
       billactions = billactions + "<li>" + contents.objects[i].major_actions[j][2] + "</li>"
     }
-    if(contents.objects[i].display_number.substr(0,1) == "S"){
+    if(contents.objects[i].bill_type == "senate_bill"){
       senatetext = senatetext + "<h3>" + contents.objects[i].display_number + ": " + contents.objects[i].current_status_label + " as of "+ contents.objects[i].current_status_date + ", introduced " + contents.objects[i].introduced_date + ".</h3><h4>" + contents.objects[i].title_without_number + "</h4><ul>" + billactions + "</ul>";
     }
-    else if(contents.objects[i].display_number.substr(0,1) == "H"){
+    else if(contents.objects[i].bill_type == "house_bill"){
       housetext = housetext + "<h3>" + contents.objects[i].display_number + ": " + contents.objects[i].current_status_label + " as of "+ contents.objects[i].current_status_date + ", introduced " + contents.objects[i].introduced_date + ".</h3><h4>" + contents.objects[i].title_without_number + "</h4><ul>" + billactions + "</ul>";
     }
     billactions = "";
   }
 }
-function showHouse(){ //Switch page text to House bills
-  document.getElementById("data").innerHTML = housetext;
-};
-function showSenate(){ //Switch page text to Senate bills
-  document.getElementById("data").innerHTML = senatetext;
+function showBills(branchtext){
+  document.getElementById("data").innerHTML = branchtext;
 }
 getApi("piracy"); //begin script
