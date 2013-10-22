66acecce2eb142e7e9118e380a78c1eca34daf4b
Ezra Brooks <ezra.m.brooks@gmail.com>
2013-10-21 21:28:20 -0400

    way better JSON script detection
    Now loops through childNodes array to find if API script URL is present;
    if so, deletes and re-adds to refresh. If not, adds.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Bill of Bytes
 =========
 
 Web app for tracking US government technology bills.
+
+This project was started at the Drexel University Computing Academy mini-hackathon but was not completed due to time restraints. As a result, it's infrequently updated.
--- main.js
+++ main.js
@@ -1,31 +1,37 @@
-var senatetext = "";
-var housetext = "";
-var billactions = "";
+var senatetext = '';
+var housetext = '';
+var billactions = '';
 function getApi(topic){ //Retrieve JSONP API file
   var api = document.createElement('script');
   api.src = 'http://www.govtrack.us/api/v2/bill?congress=112&order_by=-current_status_date&format=jsonp&q='+topic
   //console.log(api);
-  if(document.head.lastChild != api){
-    document.head.appendChild(api);
-  };
-  senatetext = "";
-  housetext = "";
+  for(i=0;i<document.head.childNodes.length;i++){
+    if(document.head.childNodes[i] == api){
+      document.head.removeChild(document.head.childNodes[i]);
+      document.head.appendChild(api);
+      break;
+    }else if(i + 1 == document.head.childNodes.length){
+      document.head.appendChild(api);
+    }
+  }
+  senatetext = '';
+  housetext = '';
 }
 function callback(contents){ //API callback
   for(var i = 0; i < contents.objects.length; i++){
     for(var j = 0; j < contents.objects[i].major_actions.length; j++){
-      billactions = billactions + "<li>" + contents.objects[i].major_actions[j][2] + "</li>"
+      billactions = billactions + '<li>' + contents.objects[i].major_actions[j][2] + '</li>'
     }
-    if(contents.objects[i].bill_type == "senate_bill"){
-      senatetext = senatetext + "<h3>" + contents.objects[i].display_number + ": " + contents.objects[i].current_status_label + " as of "+ contents.objects[i].current_status_date + ", introduced " + contents.objects[i].introduced_date + ".</h3><h4>" + contents.objects[i].title_without_number + "</h4><ul>" + billactions + "</ul>";
+    if(contents.objects[i].bill_type == 'senate_bill'){
+      senatetext = senatetext + '<h3>' + contents.objects[i].display_number + ': ' + contents.objects[i].current_status_label + ' as of '+ contents.objects[i].current_status_date + ', introduced ' + contents.objects[i].introduced_date + '.</h3><h4>' + contents.objects[i].title_without_number + '</h4><ul>' + billactions + '</ul>';
     }
-    else if(contents.objects[i].bill_type == "house_bill"){
-      housetext = housetext + "<h3>" + contents.objects[i].display_number + ": " + contents.objects[i].current_status_label + " as of "+ contents.objects[i].current_status_date + ", introduced " + contents.objects[i].introduced_date + ".</h3><h4>" + contents.objects[i].title_without_number + "</h4><ul>" + billactions + "</ul>";
+    else if(contents.objects[i].bill_type == 'house_bill'){
+      housetext = housetext + '<h3>' + contents.objects[i].display_number + ': ' + contents.objects[i].current_status_label + ' as of '+ contents.objects[i].current_status_date + ', introduced ' + contents.objects[i].introduced_date + '.</h3><h4>' + contents.objects[i].title_without_number + '</h4><ul>' + billactions + '</ul>';
     }
-    billactions = "";
+    billactions = '';
   }
 }
 function showBills(branchtext){
-  document.getElementById("data").innerHTML = branchtext;
+  document.getElementById('data').innerHTML = branchtext;
 }
-getApi("piracy"); //begin script
+getApi('piracy'); //begin script
