06ddca5e994cdcaf2972998d2ba6bf81233cbbe8
Timh26 <timh26@gmail.com>
2013-10-22 22:21:39 -0400

    continuing categorizing
    Need to continue sorting. only a few missing

--- main.js
+++ main.js
@@ -26,15 +26,21 @@ function callback(contents){ //API callback
     //categorize statuses, store to alive
     //senatetext = senatetext+that.is_current;
     if(that.bill_type == 'senate_bill'){
-      if(that.is_alive){
-        var life = "alive"
-      }else if(that.current_status == "prov_kill_veto" || that.current_status == "fail_second_senate" || that.current_status == "vetoed_override_fail_second_house" || that.current_status == "fail_originating_house" || that.current_status == "fail_second_house"){
-        //MORE VALUES NEED TO BE ADDED TO THIS LIST, OR USE A LOGIC TRICK TO COVER EVERYTHING
-        var life = "dead"
-      }else if(that.current_status == "introduced" || that.current_status == "referred"){
-        //Same as above comment
-        var life = "limbo"
+      if(['enacted_signed','passed_bill','passed_constamend','enacted_veto_override','passed_concurrentres','passed_simpleres'].indexOf(that.current_status)>-1){
+        var life = "alive";
+      // }else if(that.current_status == "prov_kill_veto" || that.current_status == "fail_second_senate" || that.current_status == "vetoed_override_fail_second_house" || that.current_status == "fail_originating_house" || that.current_status == "fail_second_house"){
+      //   //MORE VALUES NEED TO BE ADDED TO THIS LIST, OR USE A LOGIC TRICK TO COVER EVERYTHING
+      //   var life = "dead"
+      // }else if(that.current_status == "introduced" || that.current_status == "referred"){
+      //   //Same as above comment
+      //   var life = "limbo"
+      // }
+      }else if([ 'prov_kill_veto', 'fail_second_senate', 'vetoed_override_fail_second_house',"fail_originating_house","fail_second_house","vetoed_pocket",'vetoed_override_fail_originating_senate','vetoed_override_fail_second_senate','prov_kill_pingpongfail','prov_kill_suspensionfailed','fail_originating_senate'].indexOf( that.current_status ) > -1 ){
+        var life = "dead";
+      }else if(['introduced','referred'].indexOf(that.current_status)>-1){
+        var life = "limbo";
       }
+
       senatetext = senatetext +  '<div class="senate bill '+life+'"><h3>' + that.display_number + ': ' + that.current_status_label + ' as of '+ that.current_status_date + ', introduced ' + that.introduced_date + '.</h3><h4>' + that.title_without_number + '</h4><ul>' + billactions + '</ul></div>';
     }
     else if(that.bill_type == 'house_bill'){
