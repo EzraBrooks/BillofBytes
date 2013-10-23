6de99d35558680d4cf7856ad624e31b206c93838
Timh26 <timh26@gmail.com>
2013-10-23 16:57:23 -0400

    Fancy collapsing boxes for bills. Search bar

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var senatetext = '';
 var housetext = '';
 var billactions = "";
 var loadcomplete = false;
-var alive = '';
 function getApi(topic){ //Retrieve JSONP API file
   var api = document.createElement('script');
   api.src = 'http://www.govtrack.us/api/v2/bill?congress=112&order_by=-current_status_date&format=jsonp&q='+topic
@@ -18,24 +17,30 @@ function getApi(topic){ //Retrieve JSONP API file
   housetext = '';
 }
 function callback(contents){ //API callback
+  var sencount = 0;
+  var housecount=0;
   for(var i = 0; i < contents.objects.length; i++){
+    var classes = '';
     var that = contents.objects[i]
-    for(var j = 0; j < that.major_actions.length; j++){
-      billactions = billactions + '<li>' + that.major_actions[j][2] + '</li>'
+    for(var j = 0; j < that.major_actions.length && j<5; j++){
+      billactions = billactions + '<li>' + that.major_actions[j][2] + '</li>';
     }
-    //categorize statuses, store to variable life
+    //categorize statuses, store to variable classes
     if(['enacted_signed','passed_bill','passed_constamend','enacted_veto_override','passed_concurrentres','passed_simpleres'].indexOf(that.current_status)>-1){
-      var life = "alive";
+      classes = classes + "alive";
     }else if([ 'prov_kill_veto', 'fail_second_senate', 'vetoed_override_fail_second_house',"fail_originating_house","fail_second_house","vetoed_pocket",'vetoed_override_fail_originating_senate','vetoed_override_fail_second_senate','prov_kill_pingpongfail','prov_kill_suspensionfailed','fail_originating_senate'].indexOf( that.current_status ) > -1 ){
-      var life = "dead";
+      classes = classes + "dead";
     }else if(['introduced','pass_over_senate','pass_over_house', 'referred','pass_back_senate','pass_back_house','override_pass_over_house','override_pass_over_senate','reported'].indexOf(that.current_status)>-1){
-      var life = "limbo";
+      classes = classes + "limbo";
     }
+
     if(that.bill_type == 'senate_bill'){
-      senatetext = senatetext +  '<div class="bill '+life+'"><h3>' + that.display_number + ': ' + that.current_status_label + ' as of '+ that.current_status_date + ', introduced ' + that.introduced_date + '.</h3><h4>' + that.title_without_number + '</h4><ul>' + billactions + '</ul></div>';
+      senatetext = senatetext +  '<div class="bill '+classes+'"><a onclick="showMore(this);" id="expand"><img src="img/expand.png"></a><h3>' + that.display_number + ': ' + that.current_status_label + ' as of '+ that.current_status_date + ', introduced ' + that.introduced_date + '.</h3><h4>' + that.title_without_number + '</h4><ul>' + billactions + '</ul></div>';
+      sencount ++;
     }
     else if(that.bill_type == 'house_bill'){
-      housetext = housetext + '<div class="bill '+life+'"><h3>' + that.display_number + ': ' + that.current_status_label + ' as of '+ that.current_status_date + ', introduced ' + that.introduced_date + '.</h3><h4>' + that.title_without_number + '</h4><ul>' + billactions + '</ul></div>';
+      housetext = housetext + '<div class="bill '+classes+'"><a onclick=showMore('+housecount+') id="expand"><img src="img/expand.png"></a><h3>' + that.display_number + ': ' + that.current_status_label + ' as of '+ that.current_status_date + ', introduced ' + that.introduced_date + '.</h3><h4>' + that.title_without_number + '</h4><ul>' + billactions + '</ul></div>';
+      housecount++;
     }
     //billactions = '';
   }
@@ -46,5 +51,17 @@ function showBills(branchtext){
 }
 function init(){ //begin script
   getApi('piracy');
+}
+function search(){
+  var input = document.getElementById("searchBox").value;
+  getApi(input);
+}
+function showMore (item) {
+  var className = ' ' + item.parentNode.className + ' ';
+  if (className.indexOf(' tall ')>-1 ) {
+        item.parentNode.className = className.replace(' tall ', ' ');
+    } else {
+        item.parentNode.className += ' tall';
+    }
 }
 init();
