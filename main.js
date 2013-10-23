27a2685427115fdb11bc9415ade6441c0773003d
Timh26 <timh26@gmail.com>
2013-10-23 13:53:35 -0400

    Finished categorizations

--- main.js
+++ main.js
@@ -26,16 +26,9 @@ function callback(contents){ //API callback
     //categorize statuses, store to variable life
     if(['enacted_signed','passed_bill','passed_constamend','enacted_veto_override','passed_concurrentres','passed_simpleres'].indexOf(that.current_status)>-1){
       var life = "alive";
-    /*}else if(that.current_status == "prov_kill_veto" || that.current_status == "fail_second_senate" || that.current_status == "vetoed_override_fail_second_house" || that.current_status == "fail_originating_house" || that.current_status == "fail_second_house"){
-      //MORE VALUES NEED TO BE ADDED TO THIS LIST, OR USE A LOGIC TRICK TO COVER EVERYTHING
-      var life = "dead"
-    }else if(that.current_status == "introduced" || that.current_status == "referred"){
-      //Same as above comment
-      var life = "limbo"
-    }*/
     }else if([ 'prov_kill_veto', 'fail_second_senate', 'vetoed_override_fail_second_house',"fail_originating_house","fail_second_house","vetoed_pocket",'vetoed_override_fail_originating_senate','vetoed_override_fail_second_senate','prov_kill_pingpongfail','prov_kill_suspensionfailed','fail_originating_senate'].indexOf( that.current_status ) > -1 ){
       var life = "dead";
-    }else if(['introduced','referred'].indexOf(that.current_status)>-1){
+    }else if(['introduced','pass_over_senate','pass_over_house', 'referred','pass_back_senate','pass_back_house','override_pass_over_house','override_pass_over_senate','reported'].indexOf(that.current_status)>-1){
       var life = "limbo";
     }
     if(that.bill_type == 'senate_bill'){
